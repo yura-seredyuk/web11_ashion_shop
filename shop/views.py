70b10fd1b1bd42ba7772d112dfe704ea10e54c5f
Юрій Середюк <yura2590@gmail.com>
2021-12-16 21:03:08 +0200

    add: added templates

--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, 'base.html')
+    return render(request, 'pages/index.html')
+
+def shop(request):
+    return render(request, 'pages/shop.html')
+
+def product_details(request):
+    return render(request, 'pages/product-details.html')
+
+def shop_cart(request):
+    return render(request, 'pages/shop-cart.html')
+
+def checkout(request):
+    return render(request, 'pages/checkout.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
