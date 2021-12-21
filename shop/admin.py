8e91968d1f282fee97c57afb1aa4327d01f19cbd
Юрій Середюк <yura2590@gmail.com>
2021-12-21 19:43:56 +0200

    add:  added models and admin panel settings for models table displayng

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.db import models
+from .models import Category, Tag, Brand, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'country']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Brand, BrandAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Tag, TagAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'old_price', 'rait', 'available', 'quantity']
+    list_filter = ['rait', 'available', 'created', 'tag', 'brand', 'category']
+    list_editable = ['price', 'old_price', 'available', 'quantity']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Product, ProductAdmin)
+
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import SET_NULL
+from django.urls import reverse
 
 
 class Brand(models.Model):
@@ -11,14 +12,73 @@ class Brand(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reversed('shop:product_list_by_brand', args=[self.slug])
+        return reverse('shop:product_list_by_brand', args=[self.slug])
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = 'Category',
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+
+class Tag(models.Model):
+    """
+    select: Men's, Women's
+    """
+    TAGS = (
+        ('men', "Men's"),
+        ('womens', "Women's"),
+    )
+    name = models.CharField(max_length=10, choices=TAGS, default='men')
+    slug = models.SlugField(max_length=10, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_tag', args=[self.slug])
 
 class Product(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
     brand = models.ForeignKey(Brand, related_name='products', null=True, on_delete=SET_NULL)
+    tag = models.ForeignKey(Tag, related_name='products', null=True, on_delete=SET_NULL)
+    category = models.ForeignKey(Category, related_name='products', null=True, on_delete=SET_NULL)
     main_photo = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     photo_1 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     photo_2 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     photo_3 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-   
+    details = models.TextField() # short details
+    description = models.TextField()
+    specification = models.TextField()
+    # reviews
+    price = models.DecimalField(max_digits=10, decimal_places=1)
+    old_price = models.DecimalField(max_digits=10, decimal_places=1, blank=True)
+    available = models.BooleanField(default=True)
+    quantity = models.IntegerField(default=5)
+    rait = models.PositiveIntegerField(default=0)
+    review_count = models.IntegerField(default=0)
+    # colors
+    # sizes
+    promotions = models.CharField(max_length=50, default='Free shipping')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("name",)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
